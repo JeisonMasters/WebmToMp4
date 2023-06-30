7603274e1eb6b8b33335dd1188ad18136e35f328
JeisonMasters <ricardo161195@hotmail.com>
2023-06-30 11:27:48 -0600

    agregada funcion modificar cambiadas funciones abrir y leer

--- Code/main.py
+++ Code/main.py
@@ -3,11 +3,25 @@ from config.definitions import ROOT_DIR
 
 def abrirArchivo(path, flags):
     return open(path,flags)
-    
+
+#abrir en modo leer: 'r'
+def leerArchivo(archivo):
+    f = abrirArchivo(archivo, 'r')
+    print(f.read())
+    f.close()
+
+#abrir en modo append: 'a'   
+def modificarArchivo(archivo, modificacion):
+    f = abrirArchivo(archivo,'a')
+    f.write(modificacion)
+    f.close() 
 
 def main():
-    openedFile = abrirArchivo(os.path.join(ROOT_DIR,'Files', 'test.txt'),'r')
-    print (openedFile.read())
+    rutaArchivo = os.path.join(ROOT_DIR,'Files', 'test.txt')
+    leerArchivo(rutaArchivo)
+    modificarArchivo(rutaArchivo,"\nmodificacion de prueba xd")
+    leerArchivo(rutaArchivo)
+    
     
 
 if __name__ == "__main__":
