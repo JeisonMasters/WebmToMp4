578c2a7de9e766ca6fb2bd0d2c47c5356a3aa7c8
JeisonMasters <ricardo161195@hotmail.com>
2023-06-30 10:57:03 -0600

    modificado funcion open

--- Code/main.py
+++ Code/main.py
@@ -1,13 +1,14 @@
 import os
 from config.definitions import ROOT_DIR
 
-def abrirArchivo(file):
-    f = open(file)
-    return f
+def abrirArchivo(path, flags):
+    return open(path,flags)
+    
 
 def main():
-    openedFile = abrirArchivo(os.path.join(ROOT_DIR,'Files','test.txt'))
-    print ("Nombre de archivo: ",openedFile.name)
+    openedFile = abrirArchivo(os.path.join(ROOT_DIR,'Files', 'test.txt'),'r')
+    print (openedFile.read())
+    
 
 if __name__ == "__main__":
     main()
